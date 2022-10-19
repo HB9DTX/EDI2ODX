929e01294d5231e381440e88bd81e02eb1eb4441
HB9DTX <github@yvesoesch.ch>
2022-10-19 19:10:47 +0200

    Separation of functions, dictionnary for ODS/band Typos and warning removed. variable and function names modified according to coding conventions

--- edi2odx.py
+++ edi2odx.py
@@ -1,101 +1,98 @@
 import pandas as pd  # sudo apt-get install python3-pandas
 import logging
-import datetime
 
-#This dictionnary sets the distance limits in km to select the intersting QSO's (per band)
-#band identifier according to EDI format spec for PBand argument.
-ODX = {'144 MHz' : 700, '432 MHz' : 500, '1,3 GHz' : 300}
 
-def readEdiFile(filename):
-    #with open('HB9XC_432.edi', 'r') as ediFile:
-    with open(filename, 'r') as ediFile:
-    #https://stackoverflow.com/questions/63596329/python-pandas-read-csv-with-commented-header-line
+# This dictionary sets the distance limits in km to select the interesting QSO's (per band)
+# band identifier according to EDI format spec for PBand argument.
+ODX = {'144 MHz': 700, '432 MHz': 500, '1,3 GHz': 300}
+
+# Unfortunately 432 or 435 MHz exist as band definition (Wintest versus N1MM!)
+# OK1KKW and DARC definition of PBAND differ...
+# therefore entry is copied in the dictionary
+ODX['435 MHz'] = ODX['432 MHz']
+
+
+def read_edi_file(filename):
+    # with open('HB9XC_432.edi', 'r') as ediFile:
+    with open(filename, 'r', encoding="utf-8", errors="ignore") as ediFile:
+        # https://stackoverflow.com/questions/63596329/python-pandas-read-csv-with-commented-header-line
         for line in ediFile:
             if line.startswith('PCall='):
                 logging.debug('call found')
                 logging.debug(line)
-                call = line[6:-1]
-                logging.info('The station call is: %s', call)
+                call_sign = line[6:-1]
+                logging.info('The station call sign is: %s', call_sign)
 
             if line.startswith('PBand='):
                 logging.debug('Band found')
                 logging.debug(line)
                 band = line[6:-1]
-                bandEDI = band              #To be used for selecting the ODX distance in dictionary
-                bandFileName = band.replace(' ','')    #To be used in thefilenames (no space, no comma)
-                bandFileName = bandFileName.replace(',','_')
-                logging.info('The operating band is: %s', bandEDI)
+                band_edi = band  # To be used for selecting the ODX distance in dictionary
+                band_file_name = band.replace(' ', '')  # To be used in the filenames (no space, no comma)
+                band_file_name = band_file_name.replace(',', '_')
+                logging.info('The operating band is: %s', band_edi)
 
             else:
                 if line.startswith('[QSORecords'):
                     logging.debug('QSO log starts here')
-                    logging.debug(line)
                     break
-        QSOs = pd.read_csv(ediFile, delimiter=";", skiprows=1)
-        QSOs.columns = ['DATE', 'TIME', 'CALL', 'MODE', 'SENT_RST', 'SENT_NR', \
-                        'RECEIVED_RST', 'RECEIVED_NUMBER', \
-                        'EXCHANGE', 'LOCATOR', 'QRB', \
-                        'N_EXCH', 'N_LOCATOR', 'N_DXCC', 'DUPE']
-        #EDI file format
+        qsos_list = pd.read_csv(ediFile, delimiter=";", skiprows=1)
+        qsos_list.columns = ['DATE', 'TIME', 'CALL', 'MODE', 'SENT_RST', 'SENT_NR',
+                             'RECEIVED_RST', 'RECEIVED_NUMBER',
+                             'EXCHANGE', 'LOCATOR', 'QRB',
+                             'N_EXCH', 'N_LOCATOR', 'N_DXCC', 'DUPE']
+        # Column names matching EDI file format specification
 
     ediFile.close()
-    return(QSOs, call, bandEDI, bandFileName)
+    return qsos_list, call_sign, band_edi, band_file_name
 
 
-def selectODXonly(QSOs, ODX):
+def select_odx_only(qsos, distance_limit):
+    qsos_dx = qsos[qsos['QRB'] > distance_limit].copy()  # .copy needed to avoid SettingWithCopyWarning
+    logging.debug(qsos_dx)
 
-    QSOs_DX = QSOs[QSOs['QRB'] > ODX].copy()        #.copy needed to avoid SettingWithCopyWarning
+    qsos_dx.drop(columns=['MODE', 'SENT_RST', 'SENT_NR', 'RECEIVED_RST', 'RECEIVED_NUMBER',
+                          'EXCHANGE', 'N_EXCH', 'N_LOCATOR', 'N_DXCC', 'DUPE'], inplace=True)
 
-    logging.debug(QSOs_DX)
+    qsos_dx['DATE'] = pd.to_datetime(qsos_dx['DATE'], format='%y%m%d')
+    qsos_dx['DATE'] = qsos_dx['DATE'].dt.strftime('%Y-%m-%d')
 
-    QSOs_DX.drop(columns=['MODE', 'SENT_RST', 'SENT_NR', 'RECEIVED_RST', 'RECEIVED_NUMBER', \
-                        'EXCHANGE', 'N_EXCH', 'N_LOCATOR', 'N_DXCC', 'DUPE'], inplace=True)
+    qsos_dx['TIME'] = pd.to_datetime(qsos_dx['TIME'], format='%H%M')
+    qsos_dx['TIME'] = qsos_dx['TIME'].dt.strftime('%H:%M')
 
+    nr_qso_dx = qsos_dx.shape[0]
+    logging.debug(nr_qso_dx)
 
-    #QSO_DX['newDate'] = pd.to_datetime(QSO_DX.checkin).dt.strftime("%m/%d/%Y")
-
-    QSOs_DX['DATE'] = pd.to_datetime(QSOs_DX['DATE'], format='%y%m%d')
-    QSOs_DX['DATE'] =  QSOs_DX['DATE'].dt.strftime('%Y-%m-%d')
-
-    QSOs_DX['TIME'] = pd.to_datetime(QSOs_DX['TIME'], format='%H%M')
-    QSOs_DX['TIME'] =  QSOs_DX['TIME'].dt.strftime('%H:%M')
-
-    #QSOs_DX['DATE'] = '20' + QSOs_DX['DATE'].astype('string')
-    #QSOs_DX['DATE'] = QSOs_DX['DATE'].apply(lambda x: x.strftime('%Y-%m-%d'))
-    #QSOs_DX['DATE'] = QSOs_DX['DATE']
-
-    #QSOs_DX['DATE'] = QSOs_DX['DATE'].to_datetime(QSOs_DX['DATE'], format='%Y%m%d')
-
-    logging.info('QSOs with distance over %s km:', ODX)
-    logging.info(QSOs_DX)
-    return(QSOs_DX)
-
-def generateXlsxCsvFiles(QSOs_DX, call, band):
-
-    OutputFileName = call + '__' + band
-
-    csvOutputFilename = OutputFileName +'.txt'
-    logging.debug(csvOutputFilename)
-    QSOs_DX.to_csv(csvOutputFilename, index=False, sep='\t')
+    logging.info('%s QSOs with distance over %s km:', nr_qso_dx, distance_limit)
+    logging.info(qsos_dx)
+    return qsos_dx
 
 
+def generate_xlsx_csv_files(qsos_dx, call_sign, band):
+    output_file_name = call_sign + '__' + band
 
-    excelOutputFileName = OutputFileName + '.xlsx'
-    logging.debug(excelOutputFileName)
-    QSOs_DX.to_excel(excelOutputFileName, index = False)      #requires intalling XlsxWriter package?
-    return()
+    csv_filename = output_file_name + '.txt'
+    logging.debug(csv_filename)
+    qsos_dx.to_csv(csv_filename, index=False, sep='\t')
 
-print('start')
+    excel_file_name = output_file_name + '.xlsx'
+    logging.debug(excel_file_name)
+    qsos_dx.to_excel(excel_file_name, index=False)  # maybe requires installing XlsxWriter package?
+    return
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.DEBUG)
+logging.info('Program START')
 logging.info('Distance limits to select the QSOs, per band: %s', ODX)
 
-[QSOs, call, bandEDI, bandFileName] = readEdiFile('HB9XC_1240.edi')
+# [QSOs, call, bandEDI, bandFileName] = read_edi_file('HB9XC_1240.edi')
+
+[QSOs, call, bandEDI, bandFileName] = read_edi_file('HELVETIA-2022_UHF@HB9N.435_MHz.EDI')
+
 logging.debug(QSOs)
-QSOs_DX = selectODXonly(QSOs, ODX[bandEDI])
+QSOs_DX = select_odx_only(QSOs, ODX[bandEDI])
 logging.debug(QSOs_DX)
-generateXlsxCsvFiles(QSOs_DX, call, bandFileName)
+generate_xlsx_csv_files(QSOs_DX, call, bandFileName)
 
-
+logging.info('Program END')
 # logging.debug(QSOs['CALL'])
-
